3a685580ffb62be756361ae49398d7032fd443fa
imt-eiaz <imteiaz@gmail.com>
2025-08-11 17:15:53 +0100

    completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+  }
+
+  function addItem() {
+    setItems((preValue) => {
+      return [...preValue, inputText];
+    });
+    setInputText("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
-        <h1>To-Do List</h1>
+        <h1>To Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input
+          value={inputText}
+          onChange={handleChange}
+          type="text"
+          placeholder="Enter note"
+          autoFocus
+        />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((addTodo) => {
+            return <li>{addTodo}</li>;
+          })}
         </ul>
       </div>
     </div>
