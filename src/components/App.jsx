f9a5b7e14cf9c9c35340ee68859882f626f3c911
imt-eiaz <imteiaz@gmail.com>
2025-08-11 23:51:47 +0100

    error on deletion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import ToDoItem from "./ToDoItem";
 
 function App() {
   const [inputText, setInputText] = useState("");
@@ -16,27 +17,36 @@ function App() {
     setInputText("");
   }
 
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.fill((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
-        <h1>To Do List</h1>
+        <h1>Todo List</h1>
       </div>
       <div className="form">
-        <input
-          value={inputText}
-          onChange={handleChange}
-          type="text"
-          placeholder="Enter note"
-          autoFocus
-        />
+        <input value={inputText} onChange={handleChange} type="text" />{" "}
         <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {items.map((addTodo) => {
-            return <li>{addTodo}</li>;
+          {items.map((todoItem, index) => {
+            return (
+              <ToDoItem
+                key={index}
+                id={index}
+                text={todoItem}
+                onChecked={deleteItem}
+              />
+            );
           })}
         </ul>
       </div>
